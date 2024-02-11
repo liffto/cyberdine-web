704519aa19402f63b22e734671e411aa2c08fa54
unknown <ajaydkrish007@gmail.com>
2024-02-11 14:05:03 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+import withPWA from "next-pwa"
+const PWA = withPWA({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+export default PWA(nextConfig);
