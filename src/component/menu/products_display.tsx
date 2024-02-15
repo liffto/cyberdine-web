185e5e13f08cc8d38f0c4c14b8537b9d0de3e080
unknown <ajaydkrish007@gmail.com>
2024-02-16 00:40:11 +0530

    fix

--- src/component/menu/products_display.tsx
+++ src/component/menu/products_display.tsx
@@ -16,7 +16,7 @@ export default function ProductDisplay({ menu }: { menu: Menu }) {
             return (
               <div key={index} >
                 <div className="px-4 md:px-0 pt-4 font-bold text-lg capitalize">{ele}</div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 pt-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 pt-4 px-4 md:px-0">
 
                 {Array.from(
                   menuData.menuMap?.get(ele)?.values() as Iterable<Item>
