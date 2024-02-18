39c9a4d3f6d8aea2cc53b2abea1dfad1c6ee7bcc
unknown <ajaydkrish007@gmail.com>
2024-02-18 21:02:01 +0530

    fix

--- src/component/menu/categories_chips.tsx
+++ src/component/menu/categories_chips.tsx
@@ -2,13 +2,13 @@ export default function CategoriesChips({list,selected}:{list:Array<string>,sele
     return (
 <div className="overflow-x-scroll py-2">
         <div className="flex gap-2 px-4">
-            {
+            {/* {
                 list && list.map((ele)=>{
                     return (
                         <div className=""></div>
                     )
                 })
-            }
+            } */}
         </div>
 </div>
     )
