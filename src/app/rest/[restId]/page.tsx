c5a800b5e7c93d8fc5ee247c01508622548d2a27
unknown <ajaydkrish007@gmail.com>
2024-03-02 11:58:57 +0530

    fix

--- src/app/rest/[restId]/page.tsx
+++ src/app/rest/[restId]/page.tsx
@@ -11,7 +11,7 @@ export default async function OrgProductsPage({
       process.env.NODE_ENV == "development"
         ? "http://localhost:3000"
         : "https://lt-menu.vercel.app"
-    }/api/rest/${params.restId}/detail`
+    }/api/rest/${params.restId}/detail`,{next:{revalidate:60*60*2}}
   );
   const json = await response.json();
   console.log(json, json.data);
