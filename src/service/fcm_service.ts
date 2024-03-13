7b11c67468a68adf95fbab879f28fe5116b814ed
tamilarasu18 <tamilsa93@gmail.com>
2024-03-13 23:31:02 +0530

    fcm fixes

--- src/service/fcm_service.ts
+++ src/service/fcm_service.ts
@@ -5,8 +5,14 @@ export class FcmService {
 
     async fcmTopic(body: any): Promise<any | null> {
         try {
+            const headers = {
+                "Access-Control-Allow-Origin": "https://www.cyberdine.in",
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization"
+            };
+
             console.log("body", body);
-            let response = await axios.post(`${process.env.NODE_ENV =="development"?"http://localhost:3000":"https://cyberdine.in"}/api/request/fcm`, body);
+            let response = await axios.post(`${process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://cyberdine.in"}/api/request/fcm`, body, { headers });
             console.log("response.status", response.status);
 
             if (response.status! >= 200 && response.status! <= 299 && response.data) {
