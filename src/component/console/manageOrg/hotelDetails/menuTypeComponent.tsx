f987b24adf38f65e00eedf651104ea81607564f3
liffto <lifftotech@gmail.com>
2024-10-31 18:31:45 +0530

    application error solved in menu page

--- src/component/console/manageOrg/hotelDetails/menuTypeComponent.tsx
+++ src/component/console/manageOrg/hotelDetails/menuTypeComponent.tsx
@@ -10,6 +10,10 @@ export default function MenuTypeComponents({ onBackClicked, onCardClicked }: { o
         localStorage.setItem("orgMenuType", orgMenuType);
         onCardClicked();
     }
+    const enableDrinksMenu = (type: string) => {
+        return (JSON.parse(localStorage.getItem("restaurantData")!).businessType == 'restroBar' && type == "drinksMenu" || type == "foodMenu")
+    }
+
     return <div className="">
         <div className="flex items-center mb-4 mt-8">
             <button onClick={handleBackClick} className="mr-2">
@@ -19,7 +23,7 @@ export default function MenuTypeComponents({ onBackClicked, onCardClicked }: { o
         </div>
         <div className="flex flex-wrap justify-start">
             {data.orgMenuType.map((dashboard, index) => (
-                <div key={index} className="flex w-full sm:w-1/2 lg:w-[28%] my-4">
+                enableDrinksMenu(dashboard.type) && <div key={index} className="flex w-full sm:w-1/2 lg:w-[28%] my-4">
                     <LandingPageCard
                         title={dashboard.title}
                         content={dashboard.content}
