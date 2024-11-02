0bcb7d42addcad0e5a97b956054a82d88c1aa8d4
liffto <lifftotech@gmail.com>
2024-11-02 08:37:27 +0530

    console added add error

--- src/service/firebase.service.ts
+++ src/service/firebase.service.ts
@@ -4,6 +4,8 @@ import { Item } from "@/model/products/items";
 import { CartMenu } from "@/model/orders/cart_menu";
 import { CustomerDetails } from "@/model/customer_detail/customer_details";
 import { FeedBackDetailsModel } from "@/model/feedback_detail/feedback_details";
+import { deleteObject, ref as uploadImage, getStorage } from "firebase/storage";
+import { FoodItem } from "@/model/manage_org_model/hotel_list";
 
 export class FirebaseServices {
     static shared: FirebaseServices = new FirebaseServices();
@@ -15,6 +17,15 @@ export class FirebaseServices {
                 callBack(snapshot.val())
             }
         })
+    }
+
+    getHotellist(callBack: Function): Unsubscribe {
+        const completedTasksRef: Query = ref(database, `hotel`);
+        return getFirebaseData(completedTasksRef, (snapshot) => {
+            if (snapshot.exists()) {
+                callBack(snapshot.val())
+            }
+        });
     }
 
     getOrgCategoryMenu(restId: string, orgMenuType: string, category: string, callBack: Function): Unsubscribe {
@@ -83,6 +94,15 @@ export class FirebaseServices {
         });
     }
 
+    getToken(callback: (data: any) => void): Unsubscribe {
+        const completedTasksRef: Query = ref(database, `githubKey`);
+        return getFirebaseData(completedTasksRef, (snapshot) => {
+            if ((snapshot.exists())) {
+                callback && callback(snapshot.val())
+            }
+        });
+    }
+
     async addFeedbackDetails(feedbackDetails: FeedBackDetailsModel, restId: string, feedbackId: string, callback?: (ele: string) => void) {
         const updateMenu = await ref(database, `/feedback/${restId}/${feedbackId}`);
         await set(updateMenu, JSON.parse(JSON.stringify(feedbackDetails))).then(() => {
@@ -117,6 +137,36 @@ export class FirebaseServices {
             console.error("Error updating isOrdered:", error);
             callback && callback("error");
         }
+    }
+
+    async updateMenuItem(selectedFoodItems: FoodItem, restId: string, orgMenuType: string, category: string, itemId: string, callback?: (ele: string) => void) {
+        const updateMenu = await ref(database, `/menu/${restId}/${orgMenuType}/${category}/${itemId}`);
+        await set(updateMenu, JSON.parse(JSON.stringify(selectedFoodItems))).then(() => {
+            callback && callback("done")
+        }).catch((error) => {
+            console.error(error);
+            callback && callback("error")
+        });
+    }
+
+    removeLogo(imageUrl: string, callBack: (success: boolean) => void): Unsubscribe {
+        const storage = getStorage();
+        const storageRef = uploadImage(storage, imageUrl);
+
+        const deleteFile = async () => {
+            try {
+                await deleteObject(storageRef);
+                callBack(true); // Notify success
+            } catch (e) {
+                console.error("Error deleting file:", e);
+                callBack(false); // Notify failure
+            }
+        };
+
+        deleteFile();
+        return () => {
+            // Optionally return a cleanup function if needed
+        };
     }
 
 
